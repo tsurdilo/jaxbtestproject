f5b835d6e9e3f4a60fb258869fdf686a047088d0
tsurdilo <tsurdilo@redhat.com>
2019-07-30 12:15:22 -0400

    added round trip test

--- src/test/java/org/test/AppTest.java
+++ src/test/java/org/test/AppTest.java
@@ -4,8 +4,12 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 import java.io.StringReader;
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
 
 import org.junit.Test;
 
@@ -47,9 +51,11 @@ public static String TEST_XML = "<process-instance>" +
 "    </variables>" +
 "</process-instance>";
 
+public static String RESULT_FROM_ROUNDTRIP = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><process-instance><process-instance-id>1</process-instance-id><variables><entry><key>myvar1</key><value xsi:type=\"xs:string\" xmlns:xs=\"http://www.w3.org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">var1Value</value></entry><entry><key>myvar2</key><value xsi:type=\"xs:string\" xmlns:xs=\"http://www.w3.org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">var1Value2</value></entry><entry><key>myvar3</key><value xsi:type=\"xs:string\" xmlns:xs=\"http://www.w3.org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">var1Value3</value></entry><entry><key>myvar4</key><value xsi:type=\"xs:string\" xmlns:xs=\"http://www.w3.org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">var1Value4</value></entry></variables></process-instance>";
+
 
     @Test
-    public void shouldAnswerWithTrue() throws Exception {
+    public void testUnmarshallFromXML() throws Exception {
         JAXBContext context = JAXBContext.newInstance(ProcessInstance.class);
         InputSource inputSource = new InputSource( new StringReader( TEST_XML ) );
         ProcessInstance pi = (ProcessInstance) context.createUnmarshaller().unmarshal(inputSource);
@@ -57,5 +63,38 @@ public static String TEST_XML = "<process-instance>" +
         assertNotNull(pi);
         assertNotNull(pi.getVariables());
         assertEquals(4, pi.getVariables().size());
+    }
+
+    @Test
+    public void testRoundtrip() throws Exception {
+        Map<String, Object> pvars = new HashMap<>();
+        pvars.put("myvar1", "var1Value");
+        pvars.put("myvar2", "var1Value2");
+        pvars.put("myvar3", "var1Value3");
+        pvars.put("myvar4", "var1Value4");
+
+
+        ProcessInstance pi = new ProcessInstance();
+        pi.setId(1L);
+        pi.setVariables(pvars);
+
+        JAXBContext context = JAXBContext.newInstance(ProcessInstance.class);
+        Marshaller m = context.createMarshaller();
+        StringWriter sw = new StringWriter();
+        m.marshal(pi, sw);
+
+        String result = sw.toString();
+
+        assertNotNull(result);
+        
+        assertEquals(RESULT_FROM_ROUNDTRIP, result  );
+
+        InputSource inputSource = new InputSource(new StringReader(result));
+        ProcessInstance pi2 = (ProcessInstance) context.createUnmarshaller().unmarshal(inputSource);
+
+        assertNotNull(pi2);
+        assertNotNull(pi2.getVariables());
+        assertEquals(4, pi2.getVariables().size());
+
     }
 }
